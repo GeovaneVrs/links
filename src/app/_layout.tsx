64e3b620e5213ededdd8f336bf920ab43430eeba
RM-1807 <geovanevalerio1807@gmail.com>
2025-02-13 17:36:42 -0300

    Finalização da construção do header

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,10 +2,13 @@ import { Stack } from "expo-router"
 import { colors } from "@/styles/colors"
 
 export default function Layout(){
+    
+    const backgroundColor = colors.gray[950]
+
     return <Stack screenOptions={
         {
             headerShown: false,
-            contentStyle: { backgroundColor: colors.gray[950]}
+            contentStyle: { backgroundColor}
         }
     }/>
 }

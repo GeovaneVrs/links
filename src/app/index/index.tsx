54377a56d1e454958e2bf9522029f4158d00b984
RM-1807 <geovanevalerio1807@gmail.com>
2025-02-14 15:45:39 -0300

    Criando a parte de Navegação

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,6 +1,6 @@
 import { View, Image, TouchableOpacity, FlatList, Modal, Text } from "react-native"
 import { MaterialIcons } from "@expo/vector-icons"
-
+import { router } from "expo-router"
 import { styles } from "./styles"
 import { colors } from "@/styles/colors"
 import { Categories } from "@/components/categories"
@@ -13,7 +13,7 @@ export default function Index(){
            <View style={styles.header}>
                 <Image source={require("@/assets/logo.png")} style={styles.logo}/>
 
-                <TouchableOpacity>
+                <TouchableOpacity onPress={() => router.navigate("/add")}>
                     <MaterialIcons name="add" size={32} color={colors.green[300]} />
                 </TouchableOpacity>
            </View>
@@ -29,7 +29,7 @@ export default function Index(){
                 showsVerticalScrollIndicator={false}
             />
 
-            <Modal transparent visible={true}>
+            <Modal transparent visible={false}>
                 <View style={styles.modal}>
                     <View style={styles.modalContent}>
                         <View style={styles.modalHeader}>
